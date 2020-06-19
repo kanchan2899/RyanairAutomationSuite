57e14941f0f2195a88498b23bcfed7bd45b9b3a5
kanchan2899 <kanchan2899@gmail.com>
2020-06-19 01:02:36 +0100

    updated a few files

--- src/test/java/com/ryanair/tests/FlightsBookingTest.java
+++ src/test/java/com/ryanair/tests/FlightsBookingTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-public class HomePageTest extends BaseClass{
+public class FlightsBookingTest extends BaseClass{
 
     private HomePage homePage;
     private OverviewPage overviewPage;
